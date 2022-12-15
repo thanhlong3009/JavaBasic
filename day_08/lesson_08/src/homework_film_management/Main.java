f47bacba484ab7f792de606d0640a1efa92b4300
thanhlong3009 <thanhlongite6@gmail.com>
2022-12-15 13:11:25 +0700

    update

--- day_08/lesson_08/src/homework_film_management/Main.java
+++ day_08/lesson_08/src/homework_film_management/Main.java
@@ -6,20 +6,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // In ra thông tin phim 
+        printInfo();
+        // Tìm phim theo tiêu đề
+        findByTitle("Bad");
+    }
+
+    public static Movie[] movies = {
+            new Movie(1, "TITANIC", "Điện ảnh lãng mạn", "James Cameron", "1-11-1997", "194 phút"),
+            new Movie(2, "CÔNG VIÊN KỶ JURA", "Viễn tưởng", "Steven Spielberg", "7-6-2013", "127 phút"),
+            new Movie(3, "HUYỀN THOẠI NGƯỜI DƠI", "Hành động", "Christopher Nolan", "19-8-2005", "140 phút"),
+    };
+
+    public static Serial[] serials = {
+            new Serial(1, "GAME OF THRONES", "Viễn tưởng", "Mark Mylod", "17-4-2011",73 ,"60 phút"),
+            new Serial(2, "THE WALKING DEAD", "Viễn tưởng", "Robert Kirkman", "12-9-2012", 177,"70 phút"),
+            new Serial(3, "BREAKING BAD", "Hình sự", " Vince Gilligan", "29-9-2010", 62,"90 phút"),
+    };
 
 
-        Movie[] movies = {
-                new Movie(1, "TITANIC", "Điện ảnh lãng mạn", "James Cameron", "1-11-1997", "194 phút"),
-                new Movie(2, "CÔNG VIÊN KỶ JURA", "Viễn tưởng", "Steven Spielberg", "7-6-2013", "127 phút"),
-                new Movie(3, "HUYỀN THOẠI NGƯỜI DƠI", "Hành động", "Christopher Nolan", "19-8-2005", "140 phút"),
-        };
-
-        Serial[] serials = {
-                new Serial(1, "GAME OF THRONES", "Viễn tưởng", "Mark Mylod", "17-4-2011",73 ,"60 phút"),
-                new Serial(2, "THE WALKING DEAD", "Viễn tưởng", "Robert Kirkman", "12-9-2012", 177,"70 phút"),
-                new Serial(3, "BREAKING BAD", "Hình sự", " Vince Gilligan", "29-9-2010", 62,"90 phút"),
-        };
-
+    public static void printInfo() {
         for (int i = 0; i < 3; i++) {
             System.out.println("Bộ phim thứ " + (i+1));
             movies[i].display();
@@ -31,19 +37,19 @@ public class Main {
             serials[i].display();
             System.out.println();
         }
+    }
 
-
-        // Tìm phim theo tiêu đề
-        String findTitle = "game";
+    // Tìm phim theo tiêu đề
+    public static void findByTitle(String title) {
         int count = 0;
         for (Movie film : movies)  {
-            if (film.getTitle().toLowerCase().contains(findTitle.toLowerCase())) {
+            if (film.getTitle().toLowerCase().contains(title.toLowerCase())) {
                 film.display();
                 count++;
             }
         }
         for (Serial film : serials) {
-            if (film.getTitle().toLowerCase().contains(findTitle.toLowerCase())) {
+            if (film.getTitle().toLowerCase().contains(title.toLowerCase())) {
                 film.display();
                 count++;
             }
@@ -52,6 +58,5 @@ public class Main {
             System.out.println("Không có bộ phim nào cần tìm");
         }
     }
-
 
 }
